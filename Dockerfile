211c87f3862ec3f82239382b90e720be37868893
TonyChou <tonychou@wondercise.com>
2023-11-22 15:07:19 +0800

    fix: workflow

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,6 @@ WORKDIR /app
 COPY requirements.txt .
 RUN pip install -r requirements.txt
 
-ARG DEMO_SECRET
-ENV DEMO_SECRET=$DEMO_SECRET
-
 COPY . .
 
 CMD [ "python3", "-m" , "flask", "run", "--host=0.0.0.0", "--port=5000"]
--- app.py
+++ app.py
@@ -1,16 +1,11 @@
 from flask import Flask
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    secret = os.getenv("DEMO_SECRET", "default name")
-    return f"Hello 1122 1456, {secret}"
+    return f"Hello 1122 1507"
 
 
 if __name__ == "__main__":
